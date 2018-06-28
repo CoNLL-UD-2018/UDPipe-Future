8e27d2558766fc25222fc8fcdfe3bc25a546cd6a
Milan Straka <milan@strakovi.com>
2018-06-28 08:15:47 +0200

    Allow specifying script variant, join logs.

--- train.sh
+++ train.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+variant="$1"; shift
+
 args=""
 while true; do
   case "$1" in
@@ -11,5 +13,5 @@ done
 for d in ud-2.2-conll18/${@:-*}/; do
   code=${d%/}
   code=${code##*/}
-  qsub -q gpu*@dll* -cwd -b y -l gpu=1,gpu_cc_min3.5=1 withcuda venv/bin/python3 ud_parser2.py $d$code "$@"
+  qsub -q gpu*@dll* -cwd -b y -l gpu=1,gpu_cc_min3.5=1 -j y withcuda venv/bin/python3 ud_parser$variant.py $d$code  $args
 done
