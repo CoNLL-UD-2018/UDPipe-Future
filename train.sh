20680b4717b58ec3a7f7c68b72c6628c56848d90
Milan Straka <milan@strakovi.com>
2018-09-13 05:42:16 +0200

    Update training script not to require ud-2.2-conll18 dir.

--- train.sh
+++ train.sh
@@ -10,8 +10,7 @@ while true; do
   break
 done
 
-for d in ud-2.2-conll18/${@:-*}/; do
-  code=${d%/}
-  code=${code##*/}
-  qsub -q gpu*@dll* -cwd -b y -l gpu=1,gpu_cc_min3.5=1 -j y withcuda venv/bin/python3 ud_parser$variant.py $d$code  $args
+for code in "$@"; do
+  d=ud-2.2-conll18/$code
+  qsub -q gpu*@dll* -cwd -b y -l gpu=1,gpu_cc_min3.5=1 -j y withcuda venv/bin/python3 ud_parser$variant.py $d/$code $args
 done
