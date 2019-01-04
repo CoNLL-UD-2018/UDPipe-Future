affbd293e0a76b75eecfe56383fa42575f2fb060
Milan Straka <milan@strakovi.com>
2019-01-04 21:38:53 +0100

    Make both venv path and tensorflow pip specification configurable.

--- venv.sh
+++ venv.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-python3 -m venv venv
-venv/bin/pip3 install tensorflow$1==1.5.0
-venv/bin/pip3 install cython
-venv/bin/pip3 install git+https://github.com/andersjo/dependency_decoding
+[ $# -ge 2 ] || { echo Usage: $0 venv_name tensorflow_pip_specification >&2; exit 1; }
+venv="$1"
+tf="$2"
+
+python3 -m venv "$venv"
+"$venv"/bin/pip3 install $tf
+"$venv"/bin/pip3 install cython
+"$venv"/bin/pip3 install git+https://github.com/andersjo/dependency_decoding
