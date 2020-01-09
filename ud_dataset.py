2bcda7969616c963a810207e60ef22a512e545fc
Joachim Wagner <jowagner@users.noreply.github.com>
2020-01-09 15:13:55 +0000

    raise error (and exit) for ill-formed lemma rule

--- ud_dataset.py
+++ ud_dataset.py
@@ -51,12 +51,7 @@ def _gen_lemma_rule(form, lemma, allow_copy):
 
 def _apply_lemma_rule(form, lemma_rule):
     if ';' not in lemma_rule:
-        import sys
-        sys.stderr.write(
-            'Warning: lemma_rule %r for form %r.'
-            ' Falling back to copying form.\n' %(lemma_rule, form)
-        )
-        return form
+        raise ValueError('lemma_rule %r for form %r missing semicolon.\n' %(lemma_rule, form))
     casing, rule = lemma_rule.split(";", 1)
     if rule.startswith("a"):
         lemma = rule[1:]
